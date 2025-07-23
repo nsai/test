7da6584de660912ab0b60a646361c0b52ecceebf
Na Sai <nsai@Nas-MacBook-Pro.local>
2025-07-23 15:23:43 -0500

    Remove top_k_frequent_words.py and test_top_k_frequent_words.py from project root

--- top_k_frequent_words.py
+++
@@ -1,7 +0,0 @@
-from typing import List
-
-def top_k_frequent_words(words: List[str], k: int) -> List[str]:
-    word_count = {}
-    for word in words:
-        word_count[word] = word_count.get(word, 0) + 1
-    return sorted(word_count.keys(), key=lambda x: (-word_count[x], x))[:k] 
